e9d0592096a247bd03c9f12aca1131261c0d5365
Angela Ricci <gericci@thecodeplayground.net>
2016-08-26 08:05:28 +0200

    Added title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta content="width=device-width, initial-scale=1.0" name="viewport" />
-	<title>[  ]</title>
+	<title>[ HTML Attributes Reference ]</title>
 
 	<link href="style.css" rel="stylesheet" type="text/css" media="all" />
 </head>
